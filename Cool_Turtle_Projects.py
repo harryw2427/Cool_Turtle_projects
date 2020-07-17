3e1cf201311c4d599edfbfd23c667a2987e30381
harryw2427 <67867112+harryw2427@users.noreply.github.com>
2020-07-17 11:04:29 -0400

    Add files via upload

---
+++ Cool_Turtle_Projects.py
@@ -0,0 +1,101 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+import turtle
+shelly=turtle.Turtle()
+shelly.shape('turtle')
+shelly.color('light blue')
+shelly.shapesize(1,0.5)
+turtle.bgcolor("blue")
+turtle.title("My Turtle Program")
+turtle.fillcolor("red")
+z=1
+x=1
+for z in range(36):
+    for x in range(6):
+        shelly.forward(26)
+        shelly.speed(65)
+        shelly.left(97)
+    shelly.right(47)
+    shelly.backward(58)
+    shelly.right(97)
+    shelly.left(62)
+
+
+# In[1]:
+
+
+import turtle
+shelly=turtle.Turtle()
+shelly.shape('turtle')
+shelly.color('light blue')
+shelly.shapesize(1,0.5)
+turtle.bgcolor("blue")
+turtle.title("My Turtle Program")
+turtle.fillcolor("red")
+turtle.pen(pencolor="purple", fillcolor="orange", pensize=10, speed=9)
+turtle.begin_fill()
+turtle.circle(90)
+turtle.end_fill()
+turtle.pen(pencolor="yellow", fillcolor="green", pensize=12, speed=8)
+turtle.begin_fill()
+turtle.circle(143)
+turtle.end_fill()
+turtle.pen(pencolor="orange", fillcolor="red", pensize=5, speed=9)
+turtle.begin_fill()
+turtle.circle(199)
+turtle.end_fill()
+turtle.pen(pencolor="red", fillcolor="black", pensize=7, speed=9)
+turtle.begin_fill()
+turtle.circle(256)
+turtle.end_fill()
+
+import turtle
+shelly=turtle.Turtle()
+shelly.shape('turtle')
+shelly.color('light purple')
+shelly.shapesize(1,0.5)
+turtle.bgcolor("yellow")
+turtle.title("My Turtle Program")
+turtle.fillcolor("green")
+
+# In[1]:
+
+
+import turtle
+shelly=turtle.Turtle()
+shelly.shape('turtle')
+shelly.color('green')
+shelly.shapesize(1,0.5)
+turtle.bgcolor("yellow")
+turtle.title("My Turtle Program")
+turtle.fillcolor("green")
+shelly = turtle.clone()
+turtle.color("magenta")
+shelly.color("red")
+turtle.circle(100)
+shelly.circle(90)
+shelly.circle(80)
+shelly.circle(70)
+shelly.circle(60)
+shelly.circle(50)
+shelly.circle(40)
+shelly.circle(30)
+shelly.circle(20)
+shelly.circle(10)
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
